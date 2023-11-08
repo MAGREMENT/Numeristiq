782003af6bad04cb3f8add2e0384f9ea4db01ad2
Zachary VANVLASSELAER <z.vanvlasselaer@student.helmo.be>
2023-11-08 23:44:05 +0100

    Change to UI is working :D

--- SudokuSolver/MainSolverPage.xaml.cs
+++ SudokuSolver/MainSolverPage.xaml.cs
@@ -2,7 +2,6 @@ using System.Windows;
 using System.Windows.Controls;
 using Model;
 using Model.Solver;
-using Model.Solver.Helpers.Changes;
 using SudokuSolver.Pages;
 using SudokuSolver.SolverOptions;
 using SudokuSolver.Utils;
@@ -35,7 +34,7 @@ public partial class MainSolverPage : IGraphicsManager, ISolverOptionHandler
         };
         Solver.CellClickedOn += (sender, row, col) =>
         {
-            LiveModification.SetCurrent(sender, row, col);
+            //TODO
         };
         Solver.SudokuAsStringChanged += ShowSudokuAsString;
         Solver.LogsUpdated += logs =>
@@ -46,18 +45,9 @@ public partial class MainSolverPage : IGraphicsManager, ISolverOptionHandler
         Solver.CurrentStrategyChanged += index =>
             StrategyList.Dispatcher.Invoke(() => StrategyList.HighlightStrategy(index));
 
-        LiveModification.LiveModified += (number, row, col, action) =>
-        {
-            if (action == ChangeType.Solution) Solver.AddDefinitiveNumber(number, row, col);
-            else if(action == ChangeType.Possibility) Solver.RemovePossibility(number, row, col);
-        };
-            
         StrategyList.InitStrategies(Solver.GetStrategies());
         StrategyList.StrategyExcluded += Solver.ExcludeStrategy;
         StrategyList.StrategyUsed += Solver.UseStrategy;
-
-        DelayBeforeSlider.Value = Solver.DelayBefore;
-        DelayAfterSlider.Value = Solver.DelayAfter;
     }
     
     public void ShowSudokuAsString(string asString)
@@ -93,45 +83,7 @@ public partial class MainSolverPage : IGraphicsManager, ISolverOptionHandler
         SolverUserControl suc = Solver;
         suc.ClearSudoku();
     }
-        
-    private void SelectedTranslationType(object sender, RoutedEventArgs e)
-    {
-        if (sender is not ComboBox box || Solver is null) return;
-        Solver.TranslationType = (SudokuTranslationType) box.SelectedIndex;
-        TranslationTypeChanged?.Invoke();
-    }
-        
-    private void SelectedOnInstanceFound(object sender, RoutedEventArgs e)
-    {
-        if (sender is not ComboBox box || Solver is null) return;
-        Solver.SetOnInstanceFound((OnInstanceFound) box.SelectedIndex);
-    }
-        
-    private void SetSolverDelayBefore(object sender, RoutedPropertyChangedEventArgs<double> e)
-    {
-        if (sender is not Slider slider) return;
-        Solver.DelayBefore = (int)slider.Value;
-    }
-        
-    private void SetSolverDelayAfter(object sender, RoutedPropertyChangedEventArgs<double> e)
-    {
-        if (sender is not Slider slider) return;
-        Solver.DelayAfter = (int)slider.Value;
-    }
 
-    private void AllowUniqueness(object sender, RoutedEventArgs e)
-    {
-        if (Solver is null) return;
-        Solver.AllowUniqueness(true);
-        StrategyList.UpdateStrategies(Solver.GetStrategies());
-    }
-
-    private void ForbidUniqueness(object sender, RoutedEventArgs e)
-    {
-        Solver.AllowUniqueness(false); 
-        StrategyList.UpdateStrategies(Solver.GetStrategies());
-    }
-    
     //ISolverOptionHandler----------------------------------------------------------------------------------------------
 
     public int DelayBefore
