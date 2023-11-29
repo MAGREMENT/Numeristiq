8e7954d58f7f6a4703111b513d1b41db05b6787f
Zachary VANVLASSELAER <z.vanvlasselaer@student.helmo.be>
2023-11-29 03:38:28 +0100

    a

--- View/Pages/StrategyManager/UserControls/StrategyUserControl.xaml.cs
+++ View/Pages/StrategyManager/UserControls/StrategyUserControl.xaml.cs
@@ -7,8 +7,6 @@ namespace View.Pages.StrategyManager.UserControls;
 
 public partial class StrategyUserControl
 {
-    private const double TotalBorderThickness = 4;
-    
     public event OnStrategyAddition? StrategyAdded;
     public event OnStrategiesInterchange? StrategiesInterchanged;
     public event OnShowAsked? ShowAsked;
@@ -31,6 +29,7 @@ public partial class StrategyUserControl
         {
             if (args.Data.GetData(typeof(StrategyShuffleData)) is not StrategyShuffleData data) return;
 
+            DefaultBorder();
             var relativePosition = args.GetPosition(this).Y > ActualHeight / 2 ? position + 1 : position;
             if (data.FromSearch) StrategyAdded?.Invoke(data.Name, relativePosition);
             else if (data.CurrentPosition != -1) StrategiesInterchanged?.Invoke(data.CurrentPosition, relativePosition);
@@ -40,7 +39,6 @@ public partial class StrategyUserControl
         DragOver += (_, args) =>
         {
             DragBorder(args.GetPosition(this).Y <= ActualHeight  / 2);
-            DefaultBorder();
         };
         DragLeave += (_, _) =>
         {
@@ -55,22 +53,19 @@ public partial class StrategyUserControl
         {
             Background = ColorManager.Background1;
         };
-        
-        DefaultBorder();
     }
 
     private void DefaultBorder()
     {
-        Border.BorderThickness = new Thickness(0, TotalBorderThickness / 2, 0, TotalBorderThickness / 2);
-        Border.BorderBrush = Brushes.Transparent;
+        UpperBorder.BorderBrush = Brushes.Transparent;
+        LowerBorder.BorderBrush = Brushes.Transparent;
     }
 
     private void DragBorder(bool upper)
     {
-        Border.BorderThickness = upper 
-            ? new Thickness(0, TotalBorderThickness, 0, 0) 
-            : new Thickness(0, 0, 0, TotalBorderThickness);
-        Border.BorderBrush = Brushes.Aqua;
+        DefaultBorder();
+        if (upper) UpperBorder.BorderBrush = Brushes.Aqua;
+        else LowerBorder.BorderBrush = Brushes.Aqua;
     }
 }
 
