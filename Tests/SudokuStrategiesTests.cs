2380816bd0afa6ca5be69f0a19f200e0b6c99726
Zachary VANVLASSELAER <z.vanvlasselaer@student.helmo.be>
2024-02-29 16:44:10 +0100

    better set up

--- Tests/SudokuStrategiesTests.cs
+++ Tests/SudokuStrategiesTests.cs
@@ -10,67 +10,49 @@ public class SudokuStrategiesTests
 {
     private readonly SudokuSolver _solver = new();
 
-    private readonly SudokuStrategyTestCase[] _testCases =
-    {
-        new(new SimpleColoringStrategy(), 
-            "05k088880h03k011210hk81121c005s00348c0032148g111c40c0h0911c00305g1210hc0030h05c0214811g188c021g111880h440c0311810h0503210941g1g105480h4881032111214803g111480h8105",
-            new SolverProgress(ProgressType.SolutionAddition, 7, 1, 4),
-            new SolverProgress(ProgressType.SolutionAddition, 7, 4, 3),
-            new SolverProgress(ProgressType.SolutionAddition, 7, 3, 8),
-            new SolverProgress(ProgressType.SolutionAddition, 7, 5, 0),
-            new SolverProgress(ProgressType.SolutionAddition, 7, 7, 2),
-            new SolverProgress(ProgressType.SolutionAddition, 7, 8, 5))
-    };
-
     [OneTimeSetUp]
     public void FirstSetUp()
     {
         _solver.ChangeBuffer = new LogManagedChangeBuffer(_solver);
     }
 
+    [TearDown]
+    public void TearDown()
+    {
+        _solver.StrategyManager.ClearStrategies();
+    }
+
     [Test]
-    public void TestCases()
+    public void SimpleColoringTest()
     {
-        foreach (var testCase in _testCases)
-        {
-            testCase.Test(_solver);
-        }
-    }
-}
-
-public class SudokuStrategyTestCase
-{
-    private readonly SudokuStrategy _strategy;
-    private readonly string _stateBefore32;
-    private readonly SolverProgress[] _expected;
-
-    public SudokuStrategyTestCase(SudokuStrategy strategy, string stateBefore32, params SolverProgress[] expected)
-    {
-        _strategy = strategy;
-        _stateBefore32 = stateBefore32;
-        _expected = expected;
+        TestSudokuStrategyInstance(new SimpleColoringStrategy(),
+            "05k088880h03k011210hk81121c005s00348c0032148g111c40c0h0911c00305g1210hc0030h05c0214811g188c021g111880h440c0311810h0503210941g1g105480h4881032111214803g111480h8105",
+            new SolverProgress(ProgressType.SolutionAddition, 7, 1, 4),
+            new SolverProgress(ProgressType.SolutionAddition, 7, 4, 3),
+            new SolverProgress(ProgressType.SolutionAddition, 7, 3, 8),
+            new SolverProgress(ProgressType.SolutionAddition, 7, 5, 0),
+            new SolverProgress(ProgressType.SolutionAddition, 7, 7, 2),
+            new SolverProgress(ProgressType.SolutionAddition, 7, 8, 5));
     }
 
-    public void Test(SudokuSolver solver)
+    private void TestSudokuStrategyInstance(SudokuStrategy strategy, string stateBefore32, params SolverProgress[] expected)
     {
-        solver.StrategyManager.AddStrategy(_strategy);
-        solver.SetState(SudokuTranslator.TranslateBase32Format(_stateBefore32, new AlphabeticalBase32Translator()));
-        solver.Solve();
+        _solver.StrategyManager.AddStrategy(strategy);
+        _solver.SetState(SudokuTranslator.TranslateBase32Format(stateBefore32, new AlphabeticalBase32Translator()));
+        _solver.Solve();
 
         List<SolverProgress> progresses = new();
-        foreach (var log in solver.Logs)
+        foreach (var log in _solver.Logs)
         {
             progresses.AddRange(log.Changes);
         }
         
         Assert.Multiple(() =>
         {
-            foreach (var progress in _expected)
+            foreach (var progress in expected)
             {
                 Assert.That(progresses, Does.Contain(progress));
             }
         });
-        
-        solver.StrategyManager.ClearStrategies();
     }
 }
